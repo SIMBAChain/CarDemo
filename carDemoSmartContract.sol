8040ce33862a900222508d7971a72453d5f2e3d5
Eric <36859647+ihave2@users.noreply.github.com>
2018-11-30 14:23:16 -0500

    Update carDemoSmartContract.sol

--- carDemoSmartContract.sol
+++ carDemoSmartContract.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.4.24;
 contract CarDealership {
    string bundleHash;
    function registerCar(string VIN, string Make, string Model, string _bundleHash, bytes32 assetId, bytes32 car) public {}
-   function accidentReport(string report_name, string assetID, string _bundleHash, bytes32 assetId, bytes32 car) public {}
-   function carSale(string soldTo, bytes20 amount, string assetID, string _bundleHash, bytes32 assetId, bytes32 car) public {}
+   function accidentReport(string report_name, string _bundleHash, bytes32 assetId, bytes32 car) public {}
+   function carSale(string soldTo, bytes20 amount, string _bundleHash, bytes32 assetId, bytes32 car) public {}
 }
